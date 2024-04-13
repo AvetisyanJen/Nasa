ce36be9b3e37e4738636360dcc63ce20a82e84e2
AvetisyanJen <Jenya.avetisyan.1991@mail.ru>
2024-04-13 21:50:04 +0400

    add new function

--- src/Pages/Asteroids/Asteroids.tsx
+++ src/Pages/Asteroids/Asteroids.tsx
@@ -1,8 +1,82 @@
-const Asteroids:React.FC=()=>{
-    return(
-        <>
-        <h1>asteroid</h1>
-        </>
-    )
+import React, { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
+import './Asteroids.scss';
+import { useTranslation } from "react-i18next";
+import axios from "axios";
+
+const Asteroids: React.FC = () => {
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [t] = useTranslation("global");
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    // useEffect(() => {
+    //     setError("");
+    // }, [startDate, endDate]);
+
+    const onSearch = async (startDate: Date, endDate: Date) => {
+        try {
+            const oneDay = 1000 * 60 * 60 * 24;
+            const rangeEnd = endDate.getTime();
+            const rangeStart = startDate.getTime();
+            const range = Math.ceil((rangeEnd - rangeStart) / oneDay);
+            
+            if (range <= 7) {
+                setLoading(true);
+                const startDayIso = startDate.toISOString().slice(0, 10);
+                const endDayIso = endDate.toISOString().slice(0, 10);
+                const api_key = 'YizyugfQ45fXCH8eJnV2U8gwZFnceLP5tDD5Ipcv';
+
+                const response = await axios.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDayIso}&end_date=${endDayIso}&api_key=${api_key}`);
+                const info = Object.values(response.data.near_earth_objects);
+                console.log(info);
+                setLoading(false);
+                setError(false)
+            } else {
+              
+                setError(true);
+            }
+        } catch (error) {
+            console.error(error);
+         
+            setLoading(false);
+        }
     }
-     export default Asteroids
+
+    return (
+        <div className="main">
+            <h3>{t("nearbyAsteroid.title")}</h3>
+            <h5>{t("nearbyAsteroid.info")}</h5>
+            <div className="date-section">
+                <span>{t("nearbyAsteroid.startDate")}:</span>
+                <DatePicker
+                    popperPlacement="bottom"
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date as Date)}
+                    dateFormat='dd/MM/yyyy'
+                    showYearDropdown
+                    scrollableMonthYearDropdown />
+                <span>{t("nearbyAsteroid.endDate")}:</span>
+                <DatePicker
+                    popperPlacement="bottom"
+                    selected={endDate}
+                    onChange={(date) => setEndDate(date as Date)}
+                    selectsEnd
+                    startDate={startDate}
+                    minDate={startDate}
+                    dateFormat="dd/MM/yyyy"
+                    showYearDropdown
+                    scrollableMonthYearDropdown
+                />
+                <button className="search" onClick={() => onSearch(startDate, endDate)}>{t("nearbyAsteroid.search")}</button>
+            </div>
+            {loading && <p>Loading...</p>}
+            {error && <p className="alert">{t("nearbyAsteroid.range")}</p>}
+        </div>
+    )
+}
+
+export default Asteroids;
+
--- src/Pages/PhotoDay/PhotoDay.tsx
+++ src/Pages/PhotoDay/PhotoDay.tsx
@@ -3,26 +3,52 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useTranslation } from "react-i18next";
 import "./PhotoDay.scss"
+import axios from "axios";
 
 const PhotoDay: React.FC = () => {
     const [date, setDate] = useState<Date | null>(new Date());
-    const[t]=useTranslation('global')
-    return (
-        <>
-        <div className="main">
-          <DatePicker
-            wrapperClassName="calendar"
-            popperPlacement="bottom"
-            selected={date}
-            onChange={(date: Date | null) => setDate(date)} 
-            dateFormat='dd/MM/yyyy'
-            showYearDropdown
-            scrollableMonthYearDropdown
-          />
-           <button className="search" >{t("dayPhoto.search")}</button>
-          </div>
-        </>
-      );
+    const [t] = useTranslation('global');
+    const [photoData, setPhotoData] = useState<Record<string, any> | null>(null);
 
+    const api_key = 'YizyugfQ45fXCH8eJnV2U8gwZFnceLP5tDD5Ipcv';
+
+    async function fetchPhotoDay(date: Date | null) {
+        try {
+            if (!date) return; // Защита от вызова fetchPhotoDay с null date
+            const day = date.toISOString().slice(0, 10);
+            const response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${day}`);
+            const data = response.data;
+            setPhotoData(data);
+        } catch (error) {
+            console.error(error);
+        }
     }
-     export default PhotoDay
+
+
+
+    console.log(photoData);
+
+    return (
+        <div className="main">
+            <DatePicker
+                wrapperClassName="calendar"
+                popperPlacement="bottom"
+                selected={date}
+                onChange={(date: Date | null) => setDate(date)}
+                dateFormat='dd/MM/yyyy'
+                showYearDropdown
+                scrollableMonthYearDropdown
+            />
+            <button className="search" onClick={() => { fetchPhotoDay(date) }}>{t("dayPhoto.search")}</button>
+            {photoData ?
+                <div className="content">
+                    <h1>{photoData.title}</h1>
+                    <img src={photoData.url} alt={photoData.title} />
+                    <a href={photoData.hdurl}>HD link of picture</a>
+                    <p>{photoData.explanation}</p>
+                </div> : <div />}
+        </div>
+    );
+}
+
+export default PhotoDay;
